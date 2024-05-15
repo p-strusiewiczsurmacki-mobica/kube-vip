073c1e7eea8f93095228e7c651a8fd7490a9e5a3
Patryk Strusiewicz-Surmacki <patryk-pawel.strusiewicz-surmacki@external.telekom.de>
2024-05-15 13:21:50 +0200

    Made braking changes
    Signed-off-by: Patryk Strusiewicz-Surmacki <patryk-pawel.strusiewicz-surmacki@external.telekom.de>

--- pkg/manager/watch_services.go
+++ pkg/manager/watch_services.go
@@ -112,31 +112,27 @@ func (sm *Manager) servicesWatcher(ctx context.Context, serviceFunc func(context
 				break
 			}
 
-			svcAddresses := fetchServiceAddresses(svc)
-
-			// We only care about LoadBalancer services that have been allocated an address
-			if len(svcAddresses) <= 0 {
+			// Check if we ignore this service
+			if svc.Annotations["kube-vip.io/ignore"] == "true" {
+				log.Infof("(svcs) [%s] has an ignore annotation for kube-vip", svc.Name)
 				break
 			}
 
 			svcLbClass := ""
-			// Check the loadBalancer class
 			if svc.Spec.LoadBalancerClass != nil {
 				svcLbClass = *svc.Spec.LoadBalancerClass
-				// if this isn't nil then it has been configured, check if it the kube-vip loadBalancer class
-				if svcLbClass != sm.config.LoadBalancerClassName {
-					log.Infof("(svcs) [%s] specified the loadBalancer class [%s], ignoring", svc.Name, svcLbClass)
-					break
-				}
-			} else if sm.config.LoadBalancerClassOnly && sm.config.LoadBalancerClassName != svcLbClass {
-				// if kube-vip is configured to only recognize services with kube-vip's lb class, then ignore any service that have different lb class than the one configured in kube-vip
-				log.Infof("(svcs) kube-vip configured to only recognize services with kube-vip's lb class but the service [%s] didn't specify any loadBalancer class, ignoring", svc.Name)
+			}
+
+			// Check the loadBalancer class
+			if sm.config.LoadBalancerClassOnly && svcLbClass != sm.config.LoadBalancerClassName {
+				log.Infof("(svcs) [%s] specified the loadBalancer class [%s], ignoring", svc.Name, svcLbClass)
 				break
 			}
 
-			// Check if we ignore this service
-			if svc.Annotations["kube-vip.io/ignore"] == "true" {
-				log.Infof("(svcs) [%s] has an ignore annotation for kube-vip", svc.Name)
+			svcAddresses := fetchServiceAddresses(svc)
+
+			// We only care about LoadBalancer services that have been allocated an address
+			if len(svcAddresses) <= 0 {
 				break
 			}
 
