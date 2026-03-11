9cee1d90ff7b7aec37b8639a8ff8d99a79e32b84
Patryk Strusiewicz-Surmacki <patryk.pawel.strusiewicz-surmacki@external.telekom.de>
2026-03-11 15:34:45 +0100

    Added simple debauncer for endpoints watcher
    Signed-off-by: Patryk Strusiewicz-Surmacki <patryk.pawel.strusiewicz-surmacki@external.telekom.de>

--- pkg/services/watch_endpoints.go
+++ pkg/services/watch_endpoints.go
@@ -1,8 +1,10 @@
 package services
 
 import (
+	"context"
 	"fmt"
 	"sync"
+	"time"
 
 	log "log/slog"
 
@@ -13,6 +15,48 @@ import (
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/watch"
 )
+
+type debauncer struct {
+	input  <-chan watch.Event
+	output chan watch.Event
+	closed chan any
+}
+
+func newDebauncer(input <-chan watch.Event) *debauncer {
+	return &debauncer{
+		input:  input,
+		output: make(chan watch.Event),
+		closed: make(chan any),
+	}
+}
+
+func (d *debauncer) Run(ctx context.Context) {
+	defer func() {
+		close(d.closed)
+		close(d.output)
+		fmt.Println("CLOSED DEBAUNCER CHANS")
+	}()
+	t := time.NewTicker(time.Second * 2)
+	defer t.Stop()
+	var event *watch.Event
+
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println("RETURNING FROM DEBAUNCER")
+			return
+		case tmp := <-d.input:
+			log.Debug("EVENT", "got", tmp)
+			event = &tmp
+			t.Reset(time.Second * 2)
+		case <-t.C:
+			if event != nil {
+				d.output <- *event
+				event = nil
+			}
+		}
+	}
+}
 
 func (p *Processor) watchEndpoint(svcCtx *servicecontext.Context, id string, service *v1.Service, provider providers.Provider) error {
 	log.Info("watching", "provider", provider.GetLabel(), "service_name", service.Name, "namespace", service.Namespace)
@@ -25,23 +69,34 @@ func (p *Processor) watchEndpoint(svcCtx *servicecontext.Context, id string, ser
 
 	wg := sync.WaitGroup{}
 
+	d := newDebauncer(rw.ResultChan())
+
 	defer func() {
+		<-d.closed
 		rw.Stop()
 		wg.Wait()
 	}()
 
+	dbCtx, dbCancel := context.WithCancel(svcCtx.Ctx)
+	defer func() {
+		dbCancel()
+		fmt.Println("CANCELLED DEBAUNCER CTX")
+	}()
+
+	go d.Run(dbCtx)
+
 	wg.Go(func() {
 		<-svcCtx.Ctx.Done()
 		log.Debug("context cancelled", "provider", provider.GetLabel())
+		dbCancel()
+		<-d.closed
 		rw.Stop()
 	})
-
-	ch := rw.ResultChan()
 
 	epProcessor := endpoints.NewEndpointProcessor(p.config, provider, p.bgpServer, &p.ServiceInstances, p.leaseMgr, p.TunnelMgr)
 
 	var lastKnownGoodEndpoint string
-	for event := range ch {
+	for event := range d.output {
 		// We need to inspect the event and get ResourceVersion out of it
 		switch event.Type {
 
