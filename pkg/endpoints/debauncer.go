98b144c70dbd2d1992402f959844068818ca660f
Patryk Strusiewicz-Surmacki <patryk.pawel.strusiewicz-surmacki@external.telekom.de>
2026-03-17 14:20:35 +0100

    Try with general debauncer
    Signed-off-by: Patryk Strusiewicz-Surmacki <patryk.pawel.strusiewicz-surmacki@external.telekom.de>

--- pkg/endpoints/debauncer.go
+++ pkg/endpoints/debauncer.go
@@ -5,6 +5,8 @@ import (
 	"sync"
 	"time"
 
+	v1 "k8s.io/api/core/v1"
+	discoveryv1 "k8s.io/api/discovery/v1"
 	"k8s.io/apimachinery/pkg/watch"
 	watchtools "k8s.io/client-go/tools/watch"
 )
@@ -20,6 +22,7 @@ type Debauncer struct {
 	output   chan AggregatedEvent
 	stopChan chan any
 	stopOnce sync.Once
+	events   map[string]map[string]*item
 }
 
 func NewDebauncer(rw *watchtools.RetryWatcher) *Debauncer {
@@ -28,19 +31,37 @@ func NewDebauncer(rw *watchtools.RetryWatcher) *Debauncer {
 		input:    rw.ResultChan(),
 		output:   make(chan AggregatedEvent),
 		stopChan: make(chan any),
+		events:   make(map[string]map[string]*item),
+	}
+}
+
+type item struct {
+	input     chan watch.Event
+	lastEvent *AggregatedEvent
+	output    chan<- AggregatedEvent
+	stopChan  chan any
+	stopOnce  sync.Once
+}
+
+func newItem(output chan<- AggregatedEvent) *item {
+	return &item{
+		input:    make(chan watch.Event),
+		output:   output,
+		stopChan: make(chan any),
 	}
 }
 
 const debaunceTime = time.Second * 2
 
 func (d *Debauncer) Start(ctx context.Context) {
-	t := time.NewTicker(debaunceTime)
+	wg := sync.WaitGroup{}
+	debauncerCtx, cancel := context.WithCancel(ctx)
 	defer func() {
 		close(d.output)
 		d.rw.Stop()
+		cancel()
+		wg.Wait()
 	}()
-
-	var aggregated *AggregatedEvent
 
 	for {
 		select {
@@ -52,23 +73,40 @@ func (d *Debauncer) Start(ctx context.Context) {
 			if tmp.Type == "" {
 				return
 			}
-			if aggregated != nil {
-				if tmp.Type != aggregated.Type {
-					d.output <- *aggregated
-					aggregated = nil
-				}
+
+			var namespace, name string
+			switch v := tmp.Object.(type) {
+			case *discoveryv1.EndpointSlice:
+				namespace = v.Namespace
+				name = v.Name
+			case *v1.Service:
+				namespace = v.Namespace
+				name = v.Name
+			default:
+				return
 			}
-			if aggregated == nil {
-				aggregated = &AggregatedEvent{
-					Type: tmp.Type,
-				}
+
+			nsEvent, exists := d.events[namespace]
+			if !exists {
+				d.events[namespace] = make(map[string]*item)
+				nsEvent = d.events[namespace]
 			}
-			aggregated.Events = append(aggregated.Events, &tmp)
-			t.Reset(debaunceTime)
-		case <-t.C:
-			if aggregated != nil {
-				d.output <- *aggregated
-				aggregated = nil
+
+			nameEvent, exists := nsEvent[name]
+			if !exists {
+				nsEvent[name] = newItem(d.output)
+				nameEvent = nsEvent[name]
+
+				wg.Go(func() {
+					nameEvent.start(debauncerCtx)
+					delete(d.events[namespace], name)
+				})
+			}
+
+			nameEvent.input <- tmp
+
+			if tmp.Type == watch.Deleted {
+				nameEvent.stop()
 			}
 		}
 	}
@@ -82,4 +120,49 @@ func (d *Debauncer) Stop() {
 
 func (d *Debauncer) Output() chan AggregatedEvent {
 	return d.output
+}
+
+func (i *item) start(ctx context.Context) {
+
+	t := time.NewTicker(debaunceTime)
+
+	var aggregated *AggregatedEvent
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-i.stopChan:
+			return
+		case tmp := <-i.input:
+			if aggregated != nil {
+				if tmp.Type != aggregated.Type {
+					i.output <- *aggregated
+					aggregated = nil
+				}
+			}
+			if aggregated == nil {
+				aggregated = &AggregatedEvent{
+					Type: tmp.Type,
+				}
+			}
+			aggregated.Events = append(aggregated.Events, &tmp)
+			t.Reset(debaunceTime)
+		case <-t.C:
+			if aggregated != nil {
+				i.output <- *aggregated
+				if aggregated.Type == watch.Deleted {
+
+				}
+				aggregated = nil
+			}
+			t.Reset(debaunceTime)
+		}
+	}
+}
+
+func (i *item) stop() {
+	i.stopOnce.Do(func() {
+		close(i.stopChan)
+	})
 }
